e996fb9f9e22d5d5c28298885aa65f3c9df6a930
Skitzofrenzy <skitzofrenzy2@gmail.com>
2020-03-27 04:07:55 -0400

    added basic funtionality to score and snake animaions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-    
+    function Currency(currency) {return currency.replace(/[$,]+/g,"");}
+    function GetScore() {return parseInt($("#score").text());} 
+    function AddToScore(clueAmount) {$("#score").text(GetScore()+parseInt(Currency(clueAmount)));}
+
     $(".clue").click(function(){
       $(this).addClass("selected");
-      
+        // console.log( $(this).text());
+        // console.log(GetScore())
+        let clueAmount = $(this).text();
+        AddToScore(clueAmount);
     });
   
-
 
 });
